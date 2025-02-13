0e3eff5fb585e851abab5460de3f992adbed6054
outslept <webeugene@proton.me>
2025-02-13 13:57:01 +0300

    added input validation for remap function

--- src/math.ts
+++ src/math.ts
@@ -9,6 +9,11 @@ export function lerp(min: number, max: number, t: number): number {
 }
 
 export function remap(n: number, inMin: number, inMax: number, outMin: number, outMax: number): number {
+  if (!Number.isFinite(n) || !Number.isFinite(inMin) || !Number.isFinite(inMax)
+    || +!Number.isFinite(outMin) || !Number.isFinite(outMax)) {
+    return Number.NaN
+  }
+
   if (inMin === inMax) {
     return (outMin + outMax) / 2
   }

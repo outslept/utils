3452b427d5962f4308ad84a8706a2244ef886e92
outslept <webeugene@proton.me>
2025-02-13 11:49:48 +0300

    Reverted from using arrow functions, now using plain ones
    Added isPlainObject, isArray, isMap, isSet, isDate, isRegExp, isPromise, isNull, isNullOrUndef, isNullOrUndefOrEmpty, isError, isWindow, isDocument, isFile, isBlob, isStream, isURLSearchParams, isFormData, isArrayBuffer, isBuffer, isPrimitive, isEmptyObject, isInteger, isFloat, isPositive, isNegative, isURL

--- src/is.ts
+++ src/is.ts
@@ -1,17 +1,168 @@
+import type { Buffer } from 'node:buffer'
+
 import { toString } from './base'
 
-export const isDef = <T = any>(val?: T): val is T => typeof val !== 'undefined'
-export const isBoolean = (val: any): val is boolean => typeof val === 'boolean'
-export const isFunction = <T extends Function> (val: any): val is T => typeof val === 'function'
-export const isNumber = (val: any): val is number => typeof val === 'number'
-export const isString = (val: unknown): val is string => typeof val === 'string'
-export const isObject = (val: any): val is object => toString(val) === '[object Object]'
-export const isUndefined = (val: any): val is undefined => toString(val) === '[object Undefined]'
-export const isNull = (val: any): val is null => toString(val) === '[object Null]'
-export const isRegExp = (val: any): val is RegExp => toString(val) === '[object RegExp]'
-export const isDate = (val: any): val is Date => toString(val) === '[object Date]'
+export function isDef<T = any>(val?: T): val is T {
+  return typeof val !== 'undefined'
+}
 
-// @ts-expect-error
-export const isWindow = (val: any): boolean => typeof window !== 'undefined' && toString(val) === '[object Window]'
-// @ts-expect-error
-export const isBrowser = typeof window !== 'undefined'
+export function isUndef(val: unknown): val is undefined {
+  return typeof val === 'undefined'
+}
+
+export function isBoolean(val: unknown): val is boolean {
+  return typeof val === 'boolean'
+}
+
+export function isFunction<T extends Function>(val: unknown): val is T {
+  return typeof val === 'function'
+}
+
+export function isNumber(val: unknown): val is number {
+  return typeof val === 'number' && !Number.isNaN(val)
+}
+
+export function isString(val: unknown): val is string {
+  return typeof val === 'string'
+}
+
+export function isObject(val: unknown): val is object {
+  return toString(val) === '[object Object]'
+}
+
+export function isPlainObject<T extends object = object>(val: unknown): val is T {
+  return toString(val) === '[object Object]'
+}
+
+export function isArray<T = any>(val: unknown): val is Array<T> {
+  return Array.isArray(val)
+}
+
+export function isMap<K = any, V = any>(val: unknown): val is Map<K, V> {
+  return val instanceof Map
+}
+
+export function isSet<T = any>(val: unknown): val is Set<T> {
+  return val instanceof Set
+}
+
+export function isDate(val: unknown): val is Date {
+  return val instanceof Date && !Number.isNaN(val.getTime())
+}
+
+export function isRegExp(val: unknown): val is RegExp {
+  return val instanceof RegExp
+}
+
+export function isSymbol(val: unknown): val is symbol {
+  return typeof val === 'symbol'
+}
+
+export function isPromise<T = any>(val: unknown): val is Promise<T> {
+  return isObject(val) && isFunction((val as any).then) && isFunction((val as any).catch)
+}
+
+export function isNull(val: unknown): val is null {
+  return val === null
+}
+
+export function isNullOrUndef(val: unknown): val is null | undefined {
+  return isNull(val) || isUndef(val)
+}
+
+export function isNullOrUndefOrEmpty(val: unknown): boolean {
+  if (isNullOrUndef(val)) return true
+  if (isString(val)) return val.trim().length === 0
+  if (isArray(val)) return val.length === 0
+  if (isMap(val) || isSet(val)) return val.size === 0
+  if (isObject(val)) return Object.keys(val).length === 0
+  return false
+}
+
+export function isError(val: unknown): val is Error {
+  return val instanceof Error
+}
+
+export function isWindow(val: unknown): val is Window {
+  return typeof window !== 'undefined' && toString(val) === '[object Window]'
+}
+
+export function isDocument(val: unknown): val is Document {
+  return typeof document !== 'undefined' && toString(val) === '[object HTMLDocument]'
+}
+
+export function isElement(val: unknown): val is Element {
+  return val instanceof Element
+}
+
+export function isFile(val: unknown): val is File {
+  return toString(val) === '[object File]'
+}
+
+export function isBlob(val: unknown): val is Blob {
+  return toString(val) === '[object Blob]'
+}
+
+export function isStream(val: unknown): boolean {
+  return isObject(val) && isFunction((val as any).pipe)
+}
+
+export function isURLSearchParams(val: unknown): val is URLSearchParams {
+  return toString(val) === '[object URLSearchParams]'
+}
+
+export function isFormData(val: unknown): val is FormData {
+  return toString(val) === '[object FormData]'
+}
+
+export function isArrayBuffer(val: unknown): val is ArrayBuffer {
+  return toString(val) === '[object ArrayBuffer]'
+}
+
+export function isBuffer(val: unknown): val is Buffer {
+  return val !== null && 
+         typeof val === 'object' && 
+         isFunction((val as any).write) && 
+         isFunction((val as any).fill) && 
+         isFunction((val as any).toString)
+}
+
+export function isPrimitive(val: unknown): val is string | number | boolean | symbol {
+  return (
+    isString(val) ||
+    isNumber(val) ||
+    isBoolean(val) ||
+    isSymbol(val)
+  )
+}
+
+export function isEmptyObject(val: unknown): boolean {
+  return isObject(val) && Object.keys(val).length === 0
+}
+
+export function isInteger(val: unknown): val is number {
+  return isNumber(val) && Number.isInteger(val)
+}
+
+export function isFloat(val: unknown): val is number {
+  return isNumber(val) && !Number.isInteger(val)
+}
+
+export function isPositive(val: unknown): val is number {
+  return isNumber(val) && val > 0
+}
+
+export function isNegative(val: unknown): val is number {
+  return isNumber(val) && val < 0
+}
+
+export function isURL(val: unknown): boolean {
+  if (!isString(val)) return false
+  try {
+    // eslint-disable-next-line no-new
+    new URL(val)
+    return true
+  } catch {
+    return false
+  }
+}

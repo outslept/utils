578c52a3a7b0464600404f22454442051c68676c
outslept <webeugene@proton.me>
2025-02-13 12:10:31 +0300

    Added time utils

---
+++ src/time.ts
@@ -0,0 +1,12 @@
+export const timestamp = (): number => +Date.now()
+
+export const toSeconds = (ms: number): number => Math.floor(ms / 1000)
+
+export const toMilliseconds = (s: number): number => s * 1000
+
+export const sleep = (ms: number): Promise<void> => new Promise(resolve => setTimeout(resolve, ms))
+
+export function createTimeElapsed(): () => number {
+  const start = timestamp();
+  return () => timestamp() - start;
+}

bbabdda1947bd84a21f4d8664bd9a4eb5174811b
outslept <webeugene@proton.me>
2025-02-13 13:47:23 +0300

    math utils introduced

---
+++ src/math.ts
@@ -0,0 +1,38 @@
+export function clamp(n: number, min: number, max: number): number {
+  const [minVal, maxVal] = [Math.min(min, max), Math.max(min, max)]
+  return Math.min(maxVal, Math.max(minVal, n))
+}
+
+export function lerp(min: number, max: number, t: number): number {
+  const interpolation = clamp(t, 0.0, 1.0)
+  return min + (max - min) * interpolation
+}
+
+export function remap(n: number, inMin: number, inMax: number, outMin: number, outMax: number): number {
+  if (inMin === inMax) {
+    return (outMin + outMax) / 2
+  }
+
+  return lerp(outMin, outMax, (n - inMin) / (inMax - inMin))
+}
+
+export function roundTo(n: number, decimals = 0): number {
+  const factor = 10 ** decimals
+  return Math.round((n + Number.EPSILON) * factor) / factor
+}
+
+export function randFloat(min: number, max: number): number {
+  return Math.random() * (max - min) + min
+}
+
+export function degToRad(degrees: number): number {
+  return degrees * (Math.PI / 180)
+}
+
+export function approxEquals(a: number, b: number, epsilon = 1e-6): boolean {
+  return Math.abs(a - b) < epsilon
+}
+
+export function fract(n: number): number {
+  return n - Math.trunc(n)
+}

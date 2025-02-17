b6aa75c385ee47e87c9a1519b1b36f6b42ec0751
outslept <webeugene@proton.me>
2025-02-17 17:02:59 +0300

    chore: spacing between the functions

--- src/base.ts
+++ src/base.ts
@@ -2,11 +2,14 @@ export function assert(condition: boolean, message: string): asserts condition {
   if (!condition)
     throw new Error(message)
 }
+
 export const toString = (v: any): string => Object.prototype.toString.call(v)
+
 export function getTypeName(v: any): string {
   if (v === null)
     return 'null'
   const type = toString(v).slice(8, -1).toLowerCase()
   return (typeof v === 'object' || typeof v === 'function') ? type : typeof v
 }
+
 export function noop(): void {}

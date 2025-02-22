525c72c362d06fc04e14c62bd626d797762a23eb
outslept <webeugene@proton.me>
2025-02-22 03:30:28 +0300

    feat(collection): utils for creating array of unique values

--- src/collection.ts
+++ src/collection.ts
@@ -5,11 +5,28 @@
  */
 export function groupBy<T, K extends PropertyKey>(
   arr: T[],
-  keySelector: (item: T) => K
+  keySelector: (item: T) => K,
 ): Record<K, T[]> {
   return arr.reduce((acc, item) => {
     const key = keySelector(item)
     acc[key] = [...(acc[key] || []), item]
     return acc
   }, {} as Record<K, T[]>)
+}
+
+/**
+ * Creates array of unique values
+ * @param arr - Target array
+ * @param by - Key selector for complex objects
+ */
+export function uniq<T>(arr: T[], by?: (item: T) => unknown): T[] {
+  const seen = new Set<unknown>()
+
+  return arr.filter((item) => {
+    const key = by ? by(item) : item
+    if (seen.has(key))
+      return false
+    seen.add(key)
+    return true
+  })
 }

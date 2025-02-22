7bbbb89f2f7c879e9970c2e0742bf15db657474c
outslept <webeugene@proton.me>
2025-02-22 03:50:54 +0300

    chore: jsdoc all base.ts content

--- src/base.ts
+++ src/base.ts
@@ -1,10 +1,23 @@
+/**
+ * Asserts condition is truthy
+ * @param condition - Condition to assert
+ * @param message - Error message
+ */
 export function assert(condition: boolean, message: string): asserts condition {
   if (!condition)
     throw new Error(message)
 }
 
+/**
+ * Returns string representation of value
+ * @param v - Value to convert
+ */
 export const toString = (v: any): string => Object.prototype.toString.call(v)
 
+/**
+ * Returns type name of value
+ * @param v - Value to get type name of
+ */
 export function getTypeName(v: any): string {
   if (v === null)
     return 'null'
@@ -12,4 +25,7 @@ export function getTypeName(v: any): string {
   return (typeof v === 'object' || typeof v === 'function') ? type : typeof v
 }
 
+/**
+ * No-op function
+ */
 export function noop(): void {}

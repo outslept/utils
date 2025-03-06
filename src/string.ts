2f369b46501ac0d36c8028fbb7600a79040a1e9d
outslept <webeugene@proton.me>
2025-03-07 00:43:06 +0300

    feat: caPITALIZE

--- src/string.ts
+++ src/string.ts
@@ -66,3 +66,8 @@ export function snakeCase(str: string): string {
     .replace(/[\s-]+/g, '_')
     .toLowerCase()
 }
+
+export function capitalize(str: string): string {
+  if (!str) return str;
+  return str.charAt(0).toUpperCase() + str.slice(1)
+}

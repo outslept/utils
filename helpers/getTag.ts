9b072318f4045051473c6c9bac18ef24f20049a4
outslept <webeugene@proton.me>
2025-06-17 05:29:41 +0300

    chore: inlining the variable

--- helpers/getTag.ts
+++ helpers/getTag.ts
@@ -1,11 +1,9 @@
-const { toString } = Object.prototype
-
 function getTag(value: unknown): string {
   if (value === null)
     return '[object Null]'
   if (value === undefined)
     return '[object Undefined]'
-  return toString.call(value)
+  return Object.prototype.toString.call(value)
 }
 
 export default getTag
